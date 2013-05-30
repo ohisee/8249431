fdbad2d2fb7115d4027ba1b87fc852ed6e1f89cf
ohisee <breakosin@hotmail.com>
2013-05-29 17:47:05 -0700

    Regular Expression

--- FirstPython/see/word.py
+++ FirstPython/see/word.py
@@ -10,3 +10,16 @@ def myfirst_yoursecond(p,q):
 
 print (myfirst_yoursecond("bell hooks", "curer bell"));
 
+print (len("Python is fun".split()));
+print (len("July-August 1842".split()));
+print (len("6*9==42".split()));
+
+import re;
+
+D = re.compile(r"[0-9]");
+print (D.match('05'));
+
+print (re.findall(r"[0-9]", "Mir Taqi 1723"));
+print (re.findall(r"[a-z][0-9]", "a1 2b cc3 44d"));
+print (re.findall(r"[0-9][ ][0-9]+", "a1 2b cc3 44d"));
+
