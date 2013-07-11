63f8fd18fb80b734607323c449db0716c289a4c8
ohisee <breakosin@hotmail.com>
2013-07-11 14:45:00 -0700

    Complex Parsing

--- FirstPython/token/complexparsing.py
+++ FirstPython/token/complexparsing.py
@@ -93,7 +93,8 @@ def parse(tokens,grammar):
                 # English: We're about to start parsing a "c", but
                 #  "c" may be something like "exp" with its own
                 #  production rules. We'll bring those production rules in.
-                next_states = [ (rule[0],[],rule[1],i) for rule in grammar if cd <> [] and cd[0] == rule[0] ] 
+                next_states = [ (rule[0],[],rule[1],i) 
+                               for rule in grammar if cd <> [] and cd[0] == rule[0] ] 
                 work_count = work_count + len(grammar) 
                 for next_state in next_states:
                     changes = addtoset(chart,i,next_state) or changes
@@ -107,7 +108,7 @@ def parse(tokens,grammar):
                 #  So we can parse over it and move to j+1. 
                 if cd <> [] and tokens[i] == cd[0]:
                     next_state = (x, ab + [cd[0]], cd[1:], j) 
-                changes = addtoset(chart,i+1,next_state) or changes
+                    changes = addtoset(chart,i+1,next_state) or changes
 
                 # Current State ==   x -> a b . c d , j 
                 # Option 3: If cd is [], the state is just x -> a b . , j 
@@ -117,31 +118,33 @@ def parse(tokens,grammar):
                 # English: We just finished parsing an "x" with this token,
                 #  but that may have been a sub-step (like matching "exp -> 2"
                 #  in "2+3"). We should update the higher-level rules as well. 
-                next_states = [ (jstate[0], jstate[1] + [x], (jstate[2])[1:], jstate[3] ) for jstate in chart[j] if cd == [] and jstate[2] <> [] and (jstate[2])[0] == x ]
+                next_states = [ (jstate[0], jstate[1] + [x], (jstate[2])[1:], jstate[3] ) 
+                               for jstate in chart[j] 
+                               if cd == [] and jstate[2] <> [] and (jstate[2])[0] == x ]
                 work_count = work_count + len(chart[j]) 
                 for next_state in next_states:
                     changes = addtoset(chart,i,next_state) or changes
             
-                # We're done if nothing changed!
-                if not changes: 
-                    break 
+            # We're done if nothing changed!
+            if not changes: 
+                break;
 
 # Comment this block back in if you'd like to see the chart printed. 
 # 
-#  for i in range(len(tokens)):
-#   print "== chart " + str(i)
-#   for state in chart[i]:
-#     x = state[0] 
-#     ab = state[1]
-#     cd = state[2]
-#     j = state[3] 
-#     print "    " + x + " ->",
-#     for sym in ab:
-#       print " " + sym,
-#     print " .",
-#     for sym in cd:
-#       print " " + sym,
-#     print "  from " + str(j) 
+#    for i in range(len(tokens)):
+#        print "== chart " + str(i)
+#        for state in chart[i]:
+#            x = state[0] 
+#            ab = state[1]
+#            cd = state[2]
+#            j = state[3] 
+#            print "    " + x + " ->",
+#            for sym in ab:
+#                print " " + sym,
+#            print " .",
+#            for sym in cd:
+#                print " " + sym,
+#            print "  from " + str(j) 
 
     accepting_state = (start_rule[0], start_rule[1], [], 0) 
     return accepting_state in chart[len(tokens)-1]
@@ -149,9 +152,12 @@ def parse(tokens,grammar):
 #####################################################################
 # We've rigged up a simple testing framework for you. 
 
+#from grammar3 import parse;
+
 def test_it(grammar, tokens): 
     X = max( len(grammar) , len(tokens), max([len(rule[1]) for rule in grammar]))
     result = parse(tokens,grammar) 
+    print 'Result is %s ' % result;
     print "X =", X, " work =", work_count, " 2*X^3 =", 2*X*X*X
     if work_count > 2 * X * X * X and X > 10 and X < 50:
         print "Success! Copy these down and submit them." 
@@ -167,9 +173,13 @@ grammar = [
   ("P", [ ]) , 
 ] 
 
+print ('testing...');
+
+
 for i in [5,10,15,20,25]:
     # Make i nested balanced parentheses. 
     tokens = [ "(" for j in range(i) ] + [ ")" for j in range(i) ] 
+    print tokens;
     test_it(grammar,tokens) 
 
 # If you run this and look closely, you'll see that as X doubles
