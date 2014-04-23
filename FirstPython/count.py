9efc5a504d41cd004ea40112535e1b56efb465d9
Ohisee <breakosin@hotmail.com>
2014-04-23 15:51:37 -0700

    Create count.py

---
+++ FirstPython/count.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import math;
+
+def countdown(n):
+    y = 0;
+    while x > 0:
+        x = x - 5;
+        y = y + 1;
+    print y;
+
+def time(n):
+    steps = math.ceil(n / 5.0) * 2 + 3;
+    return steps;
+
+print countdown(50);
