932f48f4118c546fa4f065e3d1ec5a79bf809810
Ohisee <breakosin@hotmail.com>
2014-04-24 15:12:30 -0700

    Update buildNodes.py
    Solutions

--- FirstPython/buildNodes.py
+++ FirstPython/buildNodes.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python
 
 def create_tour(nodes):
+    visited = [];
+    tour = [];
+    degree_count = 2;
+    for i in range(len(nodes)):
+        degree = 0;
+        current = i;
+        while degree < degree_count and (current + 1) < len(nodes):
+            if nodes[current + 1] not in visited:
+                tour.append((nodes[i], nodes[current + 1]));
+                visited.append(nodes[current + 1]);
+            degree += 1;
+            current += 1;
+    tour.append((nodes[-2], nodes[-1]));
+    return tour;
+
+def create_tour_comp(nodes):
+    # connect the first node with the second node, and second with third ..., and last with the first node
     tour = [];
     for i in range(len(nodes)):
-        a = nodes[i];
-        for n in nodes[i + 1:]:
-            tour.append((a, n));
+        tour.append((nodes[i], nodes[(i + 1) % len(nodes)]));
     return tour;
-    
-def create_tour_comp(nodes):
-    return [(nodes[i], n) for i in range(len(nodes)) for n in nodes[i + 1:]];
+
 
 if __name__ == "__main__":
-    print (create_tour([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-    print (create_tour_comp([1, 2, 3]));
+    print (create_tour([1, 2, 3, 4, 5, 6]));
+    print (create_tour_comp([1, 2, 3, 4, 5, 6]));
