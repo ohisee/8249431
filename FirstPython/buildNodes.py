12dd4dae819c23ee5fb7ba1fd3aba8e85775440c
Ohisee <breakosin@hotmail.com>
2014-04-23 18:11:54 -0700

    Update buildNodes.py

--- FirstPython/buildNodes.py
+++ FirstPython/buildNodes.py
@@ -7,6 +7,10 @@ def create_tour(nodes):
         for n in nodes[i + 1:]:
             tour.append((a, n));
     return tour;
+    
+def create_tour_comp(nodes):
+    return [(nodes[i], n) for i in range(len(nodes)) for n in nodes[i + 1:]];
 
 if __name__ == "__main__":
     print (create_tour([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+    print (create_tour_comp([1, 2, 3]));
